b2d88b44a37cb0c2f6f5749b6396314159ff08d0
MishaKhmara <1927mishakhmara03@gmail.com>
2021-03-19 16:54:12 +0200

    add scrollTo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// const basicLightbox = require('basiclightbox')
-// import * as basicLightbox from 'basiclightbox'
-
 import './styles.css';
 import PhotoApi from './apiService';
 import imageTpl from './template/imageCard.hbs';
 import LoadMoreBtn from './loadeMoreBtn';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
   hidden: true,
@@ -21,19 +20,38 @@ refs.loadMoreBtn.addEventListener('click', onLodeMore);
 function onSearch(e) {
   e.preventDefault();
   PhotoApiSertch.query = e.currentTarget.elements.query.value;
-  loadMoreBtn.show();
-
   PhotoApiSertch.resetPage();
   refs.galleryListRef.innerHTML = '';
-
-  onLodeMore();
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
+  onLodeMore()
 }
 
-function onLodeMore() {
-  loadMoreBtn.disable();
-  PhotoApiSertch.fetchPhoto().then(appendMarcup);
-  loadMoreBtn.enable();
+function onLodeMore(hits) {
+  PhotoApiSertch.fetchPhoto()
+    .then(appendMarcup)
+    .then(data => {
+      loadMoreBtn.enable();
+      window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        behavior: 'smooth',
+      });
+    })
+     
+   
 }
 function appendMarcup(hits) {
   refs.galleryListRef.insertAdjacentHTML('beforeend', imageTpl(hits));
+  if(hits.length < 12){
+   return loadMoreBtn.hide()
+  }
 }
+
+document.body.addEventListener('click', event => {
+  if (event.target.nodeName !== 'IMG') return;
+
+  const instance = basicLightbox.create(
+    `<img class="img-lightbox" src="${event.target.dataset.source}" />`,
+  );
+  instance.show();
+});
